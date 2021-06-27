65038cc41a24cdc1a99a1d37893a9c2575a47d28
deutschkihun <deutschkihun@gmail.com>
2021-06-27 23:19:39 +0200

    new project added

--- src/pages/data.js
+++ src/pages/data.js
@@ -5,7 +5,7 @@ import seminar from './common/dataimage/seminar.png'
 const projects = [
   {
     id: 1,
-    title: 'portfolio-webpage',
+    title: 'Portfolio Webpage',
     stack: 'Node.js, ExpressJS, React, MongoDB',
     img: webpage,
     desc: `Portfolio webpage is one of the best way to introduce myself in internet environment.So this project focus on the way how I can present about me with the efficient and simple method
@@ -16,7 +16,7 @@ const projects = [
   },
   {
     id: 2,
-    title : 'sport DB application',
+    title : 'Sport DB Application',
     stack: 'Javascript,React,Redux',
     img: sport,
     desc : `online shop application with frontend engineering (Javascript, React and Redux). It contains product listing, 
@@ -26,16 +26,27 @@ const projects = [
     },
     {
       id: 3,
-      title : 'project seminar: object detection in steel industry',
+      title : 'Project Seminar: object detection in steel industry',
       stack: 'tensorflow, tensorflow obeject detection API, OpenCV',
       img: seminar,
       desc : `digit detection on Ladle(metallurgy) by using Object detection neural network Faster R-CNN, corporation with FZI,IOR and Dillinger Huette `,
       completion : 'done',
       link : 'https://drive.google.com/drive/u/0/folders/1hN4a45Bf4Kmi2Qo3M4aMJNym2l_lSqka'
       },
+    {
+        id: 4,
+        title: 'Bachelor Thesis: user preference service for mobility planning',
+        stack: 'Flutter, Spring Boot',
+        img: mobility,
+        desc: `Boilerplate is a base application so that it can be used any advanced application or projects. This project aims to make a boilerplate for login and out.
+               Because login and out system is needed almost every webpage or system. So this project include basic priciple of login and out system, authentication, authorization,
+               token, cookie,session. Expectation after this project is that novice feel comfortable how to make login and out system`,
+        completion : 'done',
+        link : 'https://drive.google.com/drive/folders/1oT33NZ65bQcF4j4DcygUNtCTYgUl4GSM?usp=sharing'
+      },
    {
-    id: 4,
-    title: 'Login-out system',
+    id: 5,
+    title: 'Login/Out System',
     stack: 'Node.js, ExpressJS, React, MongoDB',
     img: login_out,
     desc: `Boilerplate is a base application so that it can be used any advanced application or projects. This project aims to make a boilerplate for login and out.
